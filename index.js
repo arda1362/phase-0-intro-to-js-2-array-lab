073ca2a72c30615b31acd9b900ebffae671f9301
Ardavan Zareirad <ardavan2525@gmail.com>
2023-03-12 01:09:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo");
+}
+function appendCat() {
+    return [...cats, "Broom"];
+}  
+
+function prependCat(name) {
+    const Cats = cats.slice();
+    Cats.unshift("Arnold");
+    return Cats;
+}
+
+function removeLastCat(name) {
+    const Cats = cats.slice();
+    Cats.pop(name);
+    return Cats;}
+
+    function removeFirstCat(name){
+        const Cats = cats.slice(); 
+        Cats.shift(name);
+         return Cats;}
+         
+
+
